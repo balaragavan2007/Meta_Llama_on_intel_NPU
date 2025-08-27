d6ac775176021a9b333b5377832899d14702b0c0
SearchSavior <etatelbaum.psc@gmail.com>
2025-08-27 18:57:07 -0400

    - added performance metrics to the chatbot - added start_chat() and finish_chat() to the chatbot

--- run.py
+++ run.py
@@ -1,16 +1,17 @@
 import openvino_genai
-import sys
 import time
 
-model_path = "Llama3B-ov"
+model_path = "/mnt/Ironwolf-4TB/Models/OpenVINO/Qwen/Qwen3-1.7B-int8_asym-ov"
 
 # Use NPU+GPU if available
-pipe = openvino_genai.LLMPipeline(model_path, "NPU")
+pipe = openvino_genai.LLMPipeline(model_path, "CPU")
 
 config = openvino_genai.GenerationConfig()
 config.max_new_tokens = 20000
 
 print("Chatbot ready! Type 'exit' to quit.\n")
+
+pipe.start_chat()
 
 while True:
     user_input = input("You: ")
@@ -24,6 +25,29 @@ while True:
         print(token_text, end="", flush=True)  # live typing
         time.sleep(0.02)  # optional delay for human-like typing effect
 
-    # Generate with streaming
-    pipe.generate(user_input, config, stream_callback)
+    # Generate with streaming and capture result to access perf_metrics
+    result = pipe.generate([user_input], config, stream_callback)
+
+    performance_report = result.perf_metrics
+
+    performance_report = {
+        'load_time (s)': round(performance_report.get_load_time() / 1000, 2), # time to load the model
+        'ttft (s)': round(performance_report.get_ttft().mean / 1000, 2), # time to first token
+        'tpot (ms)': round(performance_report.get_tpot().mean, 2), # time to process one token
+        'throughput (tokens/s)': round(performance_report.get_throughput().mean, 2), # tokens per second (throughput)
+        'generate_duration (s)': round(performance_report.get_generate_duration().mean / 1000, 2), # time to generate the response
+        'input_tokens': performance_report.get_num_input_tokens(), # number of input tokens
+        'new_tokens': performance_report.get_num_generated_tokens(), # number of new tokens generated
+        'total_tokens': performance_report.get_num_input_tokens() + performance_report.get_num_generated_tokens(), # total number of tokens
+        }
+
+    # Formatted performance metrics
+    print("\nPerformance metrics:")
+    print("-" * 40)
+    key_width = max(len(key) for key in performance_report.keys())
+    for key, value in performance_report.items():
+        print(f"  {key.ljust(key_width)} : {value}")
+
     print("\n")  # newline after response
+
+pipe.finish_chat()
